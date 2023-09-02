5011e6e6dd2fbb6087f8530703cf34b554f81a55
lady-coder-16 <ayortiz16@gmail.com>
2023-09-02 07:56:28 -0300

    Se agrega proyecto. Layout terminado

---
+++ app/src/main/java/com/curso/android/app/proyectofinal/compara_palabras/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.curso.android.app.proyectofinal.compara_palabras
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
